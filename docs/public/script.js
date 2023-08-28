ec83f95520086670075f51663e836b864ba38e81
Marvin Alegre <marvinalegre@skiff.com>
2023-08-28 11:45:27 +0800

    add stuff page

--- docs/public/script.js
+++ docs/public/script.js
@@ -1,28 +1,34 @@
 shuffle(projectsData)
 
 let body = document.body
-let projectsDisplay = document.createElement('div')
+let display = document.createElement('div')
 let navbar = document.createElement('nav')
+let navStuffButton = document.createElement('a')
 let navProjButton = document.createElement('a')
 let navAboutButton = document.createElement('a')
 let turtle = document.createElement('div')
 let hr = document.createElement('hr')
 let title = document.getElementById('title')
 
-let buildProjectsDisplay = (pd) => {
-  pd.setAttribute('id', 'project-display')
+let buildDisplay = (pd) => {
+  pd.setAttribute('id', 'display')
 }
 let buildNavbar = (nav) => {
+  navStuffButton.setAttribute('href', '/stuff')
+  navStuffButton.setAttribute('tabindex', 3)
+  navStuffButton.setAttribute('id', 'stuff-btn')
+  navStuffButton.textContent = 'STUFF'
+  nav.appendChild(navStuffButton)
+
   navProjButton.setAttribute('href', '/projects')
   navProjButton.setAttribute('tabindex', 2)
-  navProjButton.setAttribute('id', 'about-btn')
-  navProjButton.setAttribute('class', 'active')
+  navProjButton.setAttribute('id', 'projects-btn')
   navProjButton.textContent = 'PROJECTS'
   nav.appendChild(navProjButton)
 
   navAboutButton.setAttribute('href', '/')
   navAboutButton.setAttribute('tabindex', 1)
-  navAboutButton.setAttribute('id', 'projects-btn')
+  navAboutButton.setAttribute('id', 'about-btn')
   navAboutButton.textContent = 'ABOUT'
   nav.appendChild(navAboutButton)
 
@@ -40,14 +46,16 @@ let buildTurtle = (turtle) => {
   turtle.setAttribute('id', 'turtle')
 }
 
-buildProjectsDisplay(projectsDisplay)
+buildDisplay(display)
 buildNavbar(navbar)
 buildTurtle(turtle)
 
 if (window.location.pathname === '/projects') {
   title.textContent = 'Projects | Marvin Alegre'
   body.appendChild(navbar)
-  body.appendChild(projectsDisplay)
+  body.appendChild(display)
+
+  document.querySelector('#projects-btn').setAttribute('class', 'active')
 
   let tagsCount = {}
   for (let project of projectsData) {
@@ -77,11 +85,11 @@ if (window.location.pathname === '/projects') {
     filter.setAttribute('id', tag[0])
     filter.setAttribute('class', 'filter')
     filter.textContent = tag[0] + ' ' + tag[1]
-    
+
     filterButtons.appendChild(filter)
   }
-  projectsDisplay.appendChild(filterButtons)
-  
+  display.appendChild(filterButtons)
+
   // build initial project list
   let projects = document.createElement('div')
   projects.setAttribute('id', 'projects')
@@ -127,7 +135,7 @@ if (window.location.pathname === '/projects') {
 
     projects.appendChild(projectCard)
   }
-  projectsDisplay.appendChild(projects)
+  display.appendChild(projects)
 
   let filters = []
   let buttons = document.getElementsByClassName('filter')
@@ -175,8 +183,17 @@ if (window.location.pathname === '/projects') {
     })
   }
 
-  projectsDisplay.appendChild(hr)
-  projectsDisplay.appendChild(turtle)
+  display.appendChild(hr)
+  display.appendChild(turtle)
+
+} else if (window.location.pathname === '/stuff') {
+  title.textContent = 'Projects | Marvin Alegre'
+  body.appendChild(navbar)
+  body.appendChild(display)
+
+  document.querySelector('#stuff-btn').setAttribute('class', 'active')
+
+  display.appendChild(turtle)
 
 } else {
   let main = document.createElement('main')
@@ -186,10 +203,10 @@ if (window.location.pathname === '/projects') {
 
   title.textContent = '404 | Marvin Alegre'
   body.appendChild(navbar)
-  body.appendChild(projectsDisplay)
-  projectsDisplay.appendChild(main)
-  projectsDisplay.appendChild(hr)
-  projectsDisplay.appendChild(turtle)
+  body.appendChild(display)
+  display.appendChild(main)
+  display.appendChild(hr)
+  display.appendChild(turtle)
 
   document.getElementById('about-btn').style.backgroundColor = '#333'
 }
